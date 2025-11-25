83c5c0f04a1afe4933c85f30eeccc4abe8c16180
Andreas Kapp Lindquist <andkaplin05@gmail.com>
2025-11-25 18:17:41 +0100

    nvim: added config.dap and options.debug

--- nvim/init.lua
+++ nvim/init.lua
@@ -4,8 +4,10 @@ vim.o.termguicolors = true
 
 require("config.lazy")
 require("config.commands")
+require("config.dap")
 require("options.keymaps")
 require("options.options")
+require("options.debug")
 
 -- Colorscheme
 vim.cmd.colorscheme("one_monokai")
