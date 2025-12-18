8ef826c6a66c054437b4ea2f816d84ecc9b2855b
Andreas Kapp Lindquist <andkaplin05@gmail.com>
2025-12-18 12:51:26 +0100

    nvim: updated smart write to support bibtex

--- nvim/lua/options/keymaps.lua
+++ nvim/lua/options/keymaps.lua
@@ -65,15 +65,56 @@ vim.keymap.set("n", "<leader>g", "<cmd>lua _lazygit_toggle()<cr>", { desc = "Tog
 vim.keymap.set("n", "<leader>hx", function() require("hex_reader").toggle() end, { desc = "Toggle hex reader" })
 
 
--- Smart write function. Writes the current file, and compiles it if it is a tex
--- file
 local function smart_write()
     local current_file = vim.fn.expand('%:p')
-    local extension = vim.fn.expand('%:e')
+    local basename     = vim.fn.expand('%:t:r')
+    local extension    = vim.fn.expand('%:e')
+    local cwd          = vim.fn.getcwd()
+    local build_dir    = cwd .. "/build"
+
     vim.cmd("w")
-    if extension == "tex" then
-        vim.cmd('echo "Compiling latex document"')
-        vim.fn.jobstart({ "pdflatex", current_file })
+
+    if extension ~= "tex" then return end
+
+    if vim.fn.isdirectory(build_dir) == 0 then
+        vim.fn.mkdir(build_dir, "p")
+    end
+
+    vim.cmd('echo "Compiling LaTeX document into build/"')
+
+    local bib_files = vim.fn.glob("*.bib", false, true)
+    local has_bib = #bib_files > 0
+
+    -- helper to run pdflatex
+    local function run_pdflatex(callback)
+        vim.fn.jobstart({ "pdflatex", "-interaction=nonstopmode", "-output-directory="..build_dir, current_file }, {
+            on_exit = function(_, code)
+                if code ~= 0 then
+                    print("pdflatex failed")
+                elseif callback then
+                    callback()
+                end
+            end,
+        })
+    end
+
+    -- helper to run biber
+    local function run_biber(callback)
+        vim.fn.jobstart({ "biber", "--input-directory="..build_dir, "--output-directory="..build_dir, basename }, {
+            on_exit = function(_, code)
+                if code ~= 0 then
+                    print("biber failed")
+                elseif callback then
+                    callback()
+                end
+            end,
+        })
+    end
+
+    if has_bib then
+        run_pdflatex(function() run_biber(function() run_pdflatex(function() run_pdflatex() end) end) end)
+    else
+        run_pdflatex()
     end
 end
 
