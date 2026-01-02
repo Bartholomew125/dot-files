7f23a9684a6f7645318ed39506ed085d04cba8f5
Andreas Kapp Lindquist <andkaplin05@gmail.com>
2026-01-02 09:29:06 +0100

    zsh: added XDG_DATA_DIRS

--- zsh/conf.d/path.zsh
+++ zsh/conf.d/path.zsh
@@ -3,3 +3,7 @@ export PATH="$HOME/.local/share/ghcup/bin:$PATH"
 export PATH="$HOME/.local/bin:$PATH"
 export PATH="$HOME/.local/share/bin:$PATH"
 export PATH="$XDG_CONFIG_HOME/cargo/bin:$PATH"
+
+export XDG_DATA_DIRS="/usr/local/share:$XDG_DATA_DIRS"
+export XDG_DATA_DIRS="/usr/share:$XDG_DATA_DIRS"
+export XDG_DATA_DIRS="$XDG_DATA_HOME/bin:$XDG_DATA_DIRS"
