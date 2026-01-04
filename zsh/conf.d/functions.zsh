bc234145da2f9f1d8570c2ccc0149d5c99d17496
Andreas Kapp Lindquist <andkaplin05@gmail.com>
2026-01-04 15:53:38 +0100

    zsh: search history function

--- zsh/conf.d/functions.zsh
+++ zsh/conf.d/functions.zsh
@@ -4,3 +4,20 @@ open() {
     filepath=$(realpath -m "$1") || return
     hyprctl dispatch exec "xdg-open \"$filepath\"" &
 }
+
+search_command_history() {
+    local histfile="/home/andreas/.config/zsh/histfile"
+
+    local cmd
+    cmd=$(tac "$histfile" | fzf --height 40% --layout=reverse --bind 'tab:down,shift-tab:up' --no-sort)
+    if [[ -z "$cmd" ]]; then
+        zle reset-prompt
+        return
+    fi
+
+    LBUFFER+="$cmd"
+    zle reset-prompt
+    zle accept-line
+}
+zle -N search_command_history
+bindkey '^R' search_command_history
