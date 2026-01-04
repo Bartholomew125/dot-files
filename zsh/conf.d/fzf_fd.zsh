ca4433a9aa130386fbad7805141d373b0a3cef79
Andreas Kapp Lindquist <andkaplin05@gmail.com>
2026-01-04 15:48:56 +0100

    zsh: fzf fixed prompt disapperence

--- zsh/conf.d/fzf_fd.zsh
+++ zsh/conf.d/fzf_fd.zsh
@@ -1,12 +1,23 @@
 # Fuzzy finder for files and directories
 fzf_fd() {
     local file
-    file=$(find ~/. -type f -o -type d 2>/dev/null | fzf --height 40% --reverse --bind 'tab:down,shift-tab:up') || return
+    file=$(find ~/. -type f -o -type d 2>/dev/null \
+        | fzf --height 40% --reverse \
+              --bind 'tab:down,shift-tab:up' \
+              --preview='head -$LINES {}')
+
+    if [[ -z "$file" ]]; then
+        zle reset-prompt
+        return
+    fi
+    
+    
     if [[ -d "$file" ]]; then
         cd "$file" || return
     else
         ${EDITOR:-nvim} "$file"
     fi
+    zle reset-prompt
 }
 
 # Make it a ZLE widget
