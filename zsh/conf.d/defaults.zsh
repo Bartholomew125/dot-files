f7a59e4e60fea4d652281a6884aa1b15c56a9048
Andreas Kapp Lindquist <andkaplin05@gmail.com>
2026-01-05 09:28:05 +0100

    zsh: Added setup for history file

--- zsh/conf.d/defaults.zsh
+++ zsh/conf.d/defaults.zsh
@@ -1,1 +1,5 @@
 export EDITOR="/usr/bin/nvim"
+export HISTFILE="$XDG_CONFIG_HOME/zsh/histfile"
+export SAVEHIST=10000   # Number of commands to display
+export HISTSIZE=10000   # Number of commands to save
+setopt incappendhistory # Append every command to history
