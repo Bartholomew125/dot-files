c459fe2e10e9b1834c5eda246bc2b643edb51de3
Andreas Kapp Lindquist <andkaplin05@gmail.com>
2026-02-16 20:29:09 +0100

    zsh: moved fuzzyfinder to functions.zsh

--- zsh/conf.d/functions.zsh
+++ zsh/conf.d/functions.zsh
@@ -21,3 +21,30 @@ search_command_history() {
 }
 zle -N search_command_history
 bindkey '^R' search_command_history
+
+# Fuzzy finder for files and directories
+fzf_fd() {
+    local file
+    file=$(find ~/. -type f -o -type d 2>/dev/null \
+        | fzf --height 100% --reverse \
+              --bind 'tab:down,shift-tab:up' \
+              --preview='head -$LINES {}')
+
+    if [[ -z "$file" ]]; then
+        zle reset-prompt
+        return
+    fi
+    
+    
+    if [[ -d "$file" ]]; then
+        cd "$file" || return
+    else
+        ${EDITOR:-nvim} "$file"
+    fi
+    zle reset-prompt
+}
+
+# Make it a ZLE widget
+zle -N fzf_fd
+# Bind Ctrl-f to the widget
+bindkey '^f' fzf_fd
